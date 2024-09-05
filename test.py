85d449480959444e35cc21def3f65e6e4f176b9b
Ornella <igihozo.ornella@gmail.com>
2024-09-05 20:38:22 +0200

    final

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/' , methods = ['POST', 'GET'])
-def calculator()
+
+def calculator():
+    bmi = ''
+    if request.method == 'POST' and 'weight' in request.form and 'height' in request.form:
+        height = float(request.form.get('height'))
+        weight = float(request.form.get('weight'))
+        bmi = weight/((height/100) **2)
+    return render_template('index.html', bmi=bmi)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
